75cc7806f9f54cfdd180603856dac88edf7aad6d
JEFFREY MILLER OCAMPO RAZON <jeffrey.razon.college@ustangelicum.edu.ph>
2023-08-29 22:08:20 +0800

    test2

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    $("#name").text(profile.getName());
-    $("#email").text(profie.getEmail());
-    $("#image").attr('src', profile.getImageUrl());
-    $(".data").css("display", "block");
-    $(".g-signin2").css("display", "none");
-  }
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
 
-function signOut() {
+  function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-        alert("You have been signed out.");
-        $(".g-signin2").css("display", "block")
-        $(".data").css("display", "block");
+      console.log('User signed out.');
     });
   }
